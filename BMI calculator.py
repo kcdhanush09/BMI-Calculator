33106d513e52fd5ef90b54d977100cf002ffbb4f
K C DHANUSH <ch23b015@iittp.ac.in>
2025-09-03 10:31:23 +0530

    Update BMI calculator.py

--- BMI calculator.py
+++ BMI calculator.py
@@ -1,4 +1,13 @@
-x = float(input("Enter your mass: "))
-y = float(input("Enter your height: "))
+x = float(input("Enter your weight (in kg): "))
+y = float(input("Enter your height(in m): "))
 z = x/(y**2)
+
+while (x<0 or x>120):
+    print("Invalid input")
+    x = float(input("Enter your weight (in kg): "))
+
+while (y<0 or y>2.20):
+    print("Invalid input")
+    y = float(input("Enter your height(in m): "))
+
 print(f"Your BMI is {z:.2f}")
